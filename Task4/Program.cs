6ade626d575a9ba151d129b5e61fda3eb0455d03
higan758 <soohanglingkhim@gmail.com>
2025-12-04 01:11:04 +0545

    week 6 workshop tasks

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task4;
+
+public class Program
+{
+    public static void Main()
+    {
+        double price = 1000;
+
+ 
+        Console.WriteLine("Festival Discount: " +
+            Discounts.CalculateFinalPrice(price, Discounts.FestivalDiscount));
+
+        Console.WriteLine("Seasonal Discount: " +
+            Discounts.CalculateFinalPrice(price, Discounts.SeasonalDiscount));
+
+        Console.WriteLine("No Discount: " +
+            Discounts.CalculateFinalPrice(price, Discounts.NoDiscount));
+
+        Console.WriteLine("Lambda 30% Discount: " +
+            Discounts.CalculateFinalPrice(price, p => p * 0.7));
+    }
+}
---
+++ task1/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿
+namespace task1
+{
+
+    public class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        public double GetArea() => length* Breadth;
+        public double Perimeter() => 2 * (Length + Breadth);
+
+        public string showDetails() => $"length: {length}, breadth: {breadth}";
+
+    }
+
+}
