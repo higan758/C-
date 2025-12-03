406aebeac7f7f7cd4f797e88d287c8f3f9075f95
higan758 <soohanglingkhim@gmail.com>
2025-12-04 01:24:07 +0545

    Add files via upload

---
+++ week6/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task1;
+
+public class Program
+{
+    
+    public static void Main()
+    {
+        Rectangle r = new Rectangle();
+            r.Length = 10;
+            r.Breadth = 5;
+
+
+
+
+        Console.WriteLine(r.GetArea());
+        Console.WriteLine(r.Perimeter());
+        Console.WriteLine(r.showDetails());
+
+
+    }
+}
---
+++ week6/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using task2;
+
+namespace Calculations;
+
+public class Program
+{
+    public static void Main()
+    {
+        DelegateCalculation delAdd = Calculation.Add;
+        DelegateCalculation delSubstract = Calculation.Substract;
+
+        Console.WriteLine("Addition: " + delAdd(10, 5));
+        Console.WriteLine("Subtraction: " + delSubstract(10, 5));
+       
+
+    }
+}
